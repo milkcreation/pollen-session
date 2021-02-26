ce5834a374e9cfc2af3b2aa2579b0f8448c76d0c
Jordy Manner <jordy.manner@milkcreation.fr>
2021-02-26 14:18:23 +0000

    session >> 1.0.x-dev

---
+++ src/Concerns/FlashBagAwareTraitInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pollen\Session\Concerns;
+
+use Pollen\Session\FlashBagInterface;
+
+interface FlashBagAwareTraitInterface
+{
+    /**
+     * Définition|Récupération|Instance du gestionnaire de données de session éphémères.
+     *
+     * @param string|array|null $key
+     * @param mixed $default
+     *
+     * @return string|array|object|null|FlashBagInterface
+     */
+    public function flash($key = null, $default = null);
+
+    /**
+     * Définition du gestionnaire de données de session éphémères.
+     *
+     * @param FlashBagInterface $flashBag
+     *
+     * @return FlashBagAwareTrait
+     */
+    public function setFlashBag(FlashBagInterface $flashBag): FlashBagAwareTrait;
+}
